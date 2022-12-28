82df807ed9a3e034a33a793f6d4c1afd17f8a6d4
Netiar <collimow54@gmail.com>
2022-12-29 00:09:19 +0300

    rf: refactor code

--- outofbounds.cpp
+++ outofbounds.cpp
@@ -1,6 +1,8 @@
 // This C++ program compiles fine
 // as index out of bound
 // is not checked in C.
+//There is no index out of bounds checking in C/C++.
+
 #include<iostream>
 using namespace std;
 int main ()
@@ -9,5 +11,5 @@ int main ()
   
     cout << arr[3] << " ";
     cout << arr[-2] << " ";
-    
+
 }
